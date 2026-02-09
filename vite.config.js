35719bd553eff58cd7840cb7bf87f1d201d67849
Niru024 <gaikwaniranjan024@gmail.com>
2026-02-09 15:57:11 +0530

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: "/portfolio_website/"
 });
-  git config --global user.email "gaikwaniranjan024@gmail.com"
-   git config --global user.name "Niru024"
